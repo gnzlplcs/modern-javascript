38fd8d8b80196f895694dcde1805f03c0b478cfa
gnzlplcs <palaciosgm@outlook.com>
2023-02-12 21:18:44 -0500

    :construction: console log section completed

---
+++ 01-variables-data-types/01-console/script.js
@@ -0,0 +1,16 @@
+console.log(100);
+console.log(20, "hello", true);
+console.error("Alert");
+console.warn("Warning!");
+
+console.table({ name: "Brad", email: "brad@mail.com" });
+
+console.group("simple");
+console.log(1);
+console.log(2);
+console.log(3);
+console.groupEnd();
+
+const styles = "padding: 10px; background-color: white; color: green";
+
+console.log("%cHello World", styles);
