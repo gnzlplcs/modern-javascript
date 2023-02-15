76b935f1f7c6ee43b289b32f4a20cc9d946a761c
gnzlplcs <palaciosgm@outlook.com>
2023-02-14 21:11:31 -0500

    :construction: comments & shortcuts section completed

---
+++ 02-comments-shortcuts/script.js
@@ -0,0 +1,19 @@
+console.log(100);
+console.log(20, "hello", true);
+console.error("Alert");
+console.warn("Warning!");
+
+console.table({ name: "Brad", email: "brad@mail.com" });
+/*
+toggle block comment
+console.group("simple");
+console.log(1);
+console.log(2);
+console.log(3);
+console.groupEnd(); */
+
+const styles = "padding: 10px; background-color: white; color: green";
+
+console.log("%cHello World", styles);
+
+// This is a single line of code
